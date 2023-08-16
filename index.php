09141b054503809042c3d93c948c3eb5bcad00b9
Obala Joseph Ivan <josephivanobala5@gmail.com>
2023-08-17 00:13:28 +0300

    added coming soon page

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-  php_info();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coming Soon</title>
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="text-center w-full">
+    <h1 class="text-4xl font-bold">Coming Soon</h1>
+    <p class="mt-4">We're working hard to get our website ready for launch.</p>
+    <p class="mt-4">In the meantime, you can sign up for our newsletter to be notified when we go live.</p>
+    <div class="mt-4">
+      <a href="https://example.com/newsletter" class="btn btn-primary">Sign up for the newsletter</a>
+    </div>
+  </div>
+</body>
+</html>
